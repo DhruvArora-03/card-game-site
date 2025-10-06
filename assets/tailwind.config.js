e9adf6b81aeada27ae70bf42c4b7db926a1a433c
Dhruv Arora <dhruvarora.us@gmail.com>
2025-10-06 00:03:09 -0500

    create empty tailwind config to help ide show suggestions

---
+++ assets/tailwind.config.js
@@ -0,0 +1,13 @@
+// // tailwind.config.js
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./assets/js/**/*.{js,ts}",
+//     "./assets/css/**/*.{css}",
+//     "./lib/*_web/**/*.*ex",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
